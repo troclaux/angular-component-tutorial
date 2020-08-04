28ae2a1fa988d4a027bfa3ff0e19e9ce514f1670
Arthur <arthur.albuquerque.1995@gmail.com>
2020-08-04 11:07:11 -0300

    + Implementa componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ProjetoComponent } from './components/projeto/projeto.component';
+
+const routes: Routes = [
+  { path: 'projeto', component: ProjetoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
